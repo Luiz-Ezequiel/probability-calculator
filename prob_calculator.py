383aa21f31e5311c69898cc6c21931a15175a4e7
luiz <luiz.ezequiel2004@gmail.com>
2023-05-22 13:32:19 -0300

    created the function experiment that returns the probability of drawing a certain amount of collored balls from a hat with n collored balls

--- prob_calculator.py
+++ prob_calculator.py
@@ -10,10 +10,25 @@ class Hat():
 
     def draw(self, num_balls_drawn):
         balls_drawn = []
+        if num_balls_drawn > len(self.contents):
+            return self.contents
         for _ in range(num_balls_drawn):
-            index = random.randrange(len(self.contents))
-            balls_drawn.append(self.contents.pop(index))
+            index = self.contents.pop(random.choice(range(len(self.contents))))
+            balls_drawn.append(index)
         return balls_drawn
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    pass
+    m = 0
+    for _ in range(num_experiments):
+        expected_copy = copy.deepcopy(expected_balls)
+        hat_copy = copy.deepcopy(hat)
+        balls_gotten = hat_copy.draw(num_balls_drawn)
+        
+        for color in balls_gotten:
+            if color in expected_copy:
+                expected_copy[color]-= 1
+
+        if all(x <= 0 for x in expected_copy.values()):
+            m += 1
+
+    return m / num_experiments
