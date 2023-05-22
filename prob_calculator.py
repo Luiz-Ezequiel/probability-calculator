2e73c1547534b36faff26a5b1c76308fb5bea019
luiz <luiz.ezequiel2004@gmail.com>
2023-05-22 11:34:00 -0300

    created the class hat with a method called draw

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,9 +1,19 @@
 import copy
 import random
-# Consider using the modules imported above.
 
-class Hat:
-    pass
+class Hat():
+    def __init__(self, **kwargs):
+        self.contents = []
+        for keys, values in kwargs.items():
+            for _ in range(values):
+                self.contents.append(keys)
+
+    def draw(self, num_balls_drawn):
+        balls_drawn = []
+        for _ in range(num_balls_drawn):
+            index = random.randrange(len(self.contents))
+            balls_drawn.append(self.contents.pop(index))
+        return balls_drawn
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     pass
